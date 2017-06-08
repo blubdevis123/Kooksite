f4f5b3270af4894f60b926b71c9683fdd70b9aa1
jenseschouten <jense.schouten@hotmail.com>
2017-06-08 19:30:28 +0200

    Bugg fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {DashboardModule} from './dashboard/dashboard.module';
-import {UserModule} from './user/user.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    DashboardModule,
-    UserModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
